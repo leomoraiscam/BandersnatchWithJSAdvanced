83c76f05eddbf60b6a257180c7cb7e1b2ed6d06f
leomoraiscam <leozonnn@gmail.com>
2020-10-31 14:39:47 -0300

    create input e output of data

--- script.sh
+++ script.sh
@@ -4,4 +4,11 @@ for mediafile in `ls $ASSETSFOLDER | grep .mp4`; do
   INPUT=$ASSETSFOLDER/$mediafile
   FOLDER_TARGET=$ASSETSFOLDER/$FILENAME
   mkdir -p $FOLDER_TARGET
+
+  OUTPUT=$ASSETSFOLDER/$FILENAME/$FILENAME
+  DURATION=$(ffprobe -i $INPUT -show_format -v quiet | sed -n 's/duration=//p')
+
+  OUTPUT144=$OUTPUT-$DURATION-144
+  OUTPUT360=$OUTPUT-$DURATION-360
+  OUTPUT720=$OUTPUT-$DURATION-720
 done
